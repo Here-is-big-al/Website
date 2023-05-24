9df519655db3c8faf8a58164df5c76ca88898fd2
Here-is-big-al <132923760+Here-is-big-al@users.noreply.github.com>
2023-05-23 20:10:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Ya
+/*
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+    console.log("Search clicked")
+  const inputValue = searchInput.value;
+  window.open("https://alltogethernow.findhelp.com/search_results/" + inputValue, '_blank');
+});
+*/
