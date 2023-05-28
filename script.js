ebe6a17a8c02f3c49767dfddb6940871f0ea354d
here-is-big-al <alexanderaridgides@gmail.com>
2023-05-28 13:58:44 -0400

    Vs committed

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ searchButton.addEventListener('click', () => {
   window.open("https://alltogethernow.findhelp.com/search_results/" + inputValue, '_blank');
 });
 */
+const p2 = document.getElementById("p2");
+
+button.onclick = function (){p2.innerText = "Hello! This text changed"};
+
+function changeText(){p2.innerText = "Hello! This text changed"};
+
+function h6Change(){
+  document.getElementById("h6").innerText = "This h6 has been clicked";
+}
+
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
